2878a038144cc510b396efd8acf0ff456118dc6d
vaigupta4 <vaigupta4@publicisgroupe.net>
2019-11-13 15:21:21 +0000

    updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,8 @@ const config = {
   issuer: "https://publicissapientswaroop.okta.com",
   redirectUri: "http://3.11.6.45:4200/implicit/callback",
   clientId: "0oa1se230qZNZJ6Ar357",
-  pkce: false
+  authParams: {
+    responseType: ["id_token", "token"]
+  }
 };
 export default config;
