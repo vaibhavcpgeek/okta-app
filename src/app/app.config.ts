5ede695c13ba04a4f2ed9e4038076dfc05fe0c26
vaigupta4 <vaigupta4@publicisgroupe.net>
2019-11-13 14:54:20 +0000

    updated port

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 const config = {
   issuer: "https://publicissapientswaroop.okta.com",
-  redirectUri: "http://localhost:4200/implicit/callback",
+  redirectUri: "http://3.11.6.45:4200/implicit/callback",
   clientId: "0oa1se230qZNZJ6Ar357",
   pkce: true
 };
