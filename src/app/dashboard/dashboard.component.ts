0fbe847299b343707d067e3cf843884079d2d778
vaigupta4 <vaigupta4@publicisgroupe.net>
2019-11-18 10:46:23 +0000

    added logout button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { UserService } from "./../user.service";
 import { Component } from "@angular/core";
+import { OktaAuthService } from "@okta/okta-angular";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-secure",
@@ -9,10 +11,16 @@ import { Component } from "@angular/core";
 export class DashboardComponent {
   currentUser;
   today: number = Date.now();
+  isAuthenticated;
 
-  constructor(private service: UserService) {}
+  constructor(
+    private service: UserService,
+    public oktaAuth: OktaAuthService,
+    public router: Router
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
     this.getUser();
   }
 
@@ -20,5 +28,10 @@ export class DashboardComponent {
     this.service.getUserDetails().subscribe(user => {
       this.currentUser = user;
     });
+  }
+  async logout() {
+    // Terminates the session with Okta and removes current tokens.
+    await this.oktaAuth.logout();
+    this.router.navigateByUrl("/login");
   }
 }
