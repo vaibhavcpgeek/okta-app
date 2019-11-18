272fa5bf861d0698ccd501772b8e7dfb5ccc008c
vaigupta4 <vaigupta4@publicisgroupe.net>
2019-11-18 11:27:48 +0000

    updated endpoint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,6 @@ const config = {
   redirectUri: "http://3.11.6.45:4200/implicit/callback",
   clientId: "0oa1tml4ofds2Uus4357",
   pkce: false,
-  serviceUri: "https://backend.oktatechpoc.com/customers"
+  serviceUri: "https://backend.oktatechpoc.com/customer"
 };
 export default config;
