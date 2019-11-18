2e6e17c715b28ef303f40a7a462319cdcc90f2a8
vaigupta4 <vaigupta4@publicisgroupe.net>
2019-11-18 14:35:42 +0000

    disabled pkce

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ const config = {
   issuer: "https://publicissapient-online.okta.com/",
   redirectUri: "http://3.11.6.45:4200/implicit/callback",
   clientId: "0oa1tml4ofds2Uus4357",
-  pkce: true,
+  pkce: false,
   serviceUri: "https://backend.oktatechpoc.com/customer"
 };
 export default config;
